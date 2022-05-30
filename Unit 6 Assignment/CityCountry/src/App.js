9a1c48894d858c6e2d1be643b4095007686d51e0
Swapnil1296 <swapnillandage79@gmail.com>
2022-05-30 14:18:39 +0530

    updeated

--- Unit 6 Assignment/CityCountry/src/App.js
+++ Unit 6 Assignment/CityCountry/src/App.js
@@ -4,10 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import AddCountry from "./Components/AddCountry";
 import AddCity from "./Components/AddCity";
+import Navbar from "./Components/Navbar";
+
 
 function App() {
    return (
       <div className="App">
+         <Navbar/>
          <Routes>
             <Route path="/" element={<TableFn />}></Route>
             <Route path="/add-country" element={<AddCountry />}></Route>
