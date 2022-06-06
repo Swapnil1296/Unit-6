7dc944e388702f34872710f2224531368700a098
Swapnil1296 <swapnillandage79@gmail.com>
2022-06-07 00:16:57 +0530

    post req added

--- Unit 6 Assignment/Entity-Listing/Backend/src/Routes/routes.js
+++ Unit 6 Assignment/Entity-Listing/Backend/src/Routes/routes.js
@@ -5,11 +5,50 @@ const products = require('../models/producSchema');
 
 router.get('/getproducts', async (req, res) => {
   try {
-    const producstdata = await products.find();
-    // console.log(producstdata + "data mila hain");
-    res.status(201).json(producstdata);
+    //BUILD QUERY
+    // 1A) Filtering
+    const queryObj = {...req.query};
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1B) Advanced filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    let query = products.find(JSON.parse(queryString));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    const producstdata = await query
+    res.status(200).json({
+      status: 'success',
+      results: producstdata.length,
+      data: {
+        producstdata,
+      },
+    });
   } catch (error) {
-    console.log('error' + error.message);
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+});
+
+// for posing data
+router.post('/getproducts', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    return res.status(201).send(product);
+  } catch (error) {
+    return res.status(500).send(error);
   }
 });
 // getindividual
